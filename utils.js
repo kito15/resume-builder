96d8df548f6cd260fcf9a1a87a21841a02252dd9
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2025-01-26 18:13:04 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -42,8 +42,17 @@ function levenshteinDistance(str1, str2) {
     return matrix[str2.length][str1.length];
 }
 
+function calculateKeywordSimilarity(setA, setB) {
+    const a = new Set(setA);
+    const b = new Set(setB);
+    const intersection = new Set([...a].filter(x => b.has(x)));
+    const union = new Set([...a, ...b]);
+    return intersection.size / union.size;
+}
+
 module.exports = {
     normalizeText,
     generateHash,
-    calculateSimilarity
+    calculateSimilarity,
+    calculateKeywordSimilarity
 }; 
