d94e190f8b84f7f0df7d533101cc352bdef9c018
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2025-02-15 22:03:48 -0500

    Update util.js

--- config/util.js
+++ config/util.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const levenshtein = require('fast-levenshtein');
 
 function normalizeText(text) {
     return text
@@ -14,32 +15,8 @@ function generateHash(text) {
 }
 
 function calculateSimilarity(str1, str2) {
-    const longer = str1.length > str2.length ? str1 : str2;
-    const shorter = str1.length > str2.length ? str2 : str1;
-    
-    if (longer.length === 0) return 1.0;
-    
-    return (longer.length - levenshteinDistance(longer, shorter)) / longer.length;
-}
-
-function levenshteinDistance(str1, str2) {
-    const matrix = Array(str2.length + 1).fill(null)
-        .map(() => Array(str1.length + 1).fill(null));
-
-    for (let i = 0; i <= str1.length; i++) matrix[0][i] = i;
-    for (let j = 0; j <= str2.length; j++) matrix[j][0] = j;
-
-    for (let j = 1; j <= str2.length; j++) {
-        for (let i = 1; i <= str1.length; i++) {
-            const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
-            matrix[j][i] = Math.min(
-                matrix[j][i - 1] + 1,
-                matrix[j - 1][i] + 1,
-                matrix[j - 1][i - 1] + indicator
-            );
-        }
-    }
-    return matrix[str2.length][str1.length];
+    const distance = levenshtein.get(str1, str2);
+    return 1 - distance / Math.max(str1.length, str2.length);
 }
 
 function calculateKeywordSimilarity(setA, setB) {
